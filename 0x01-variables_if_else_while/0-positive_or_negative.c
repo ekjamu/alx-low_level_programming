6b29444baa3c17ee8cb20d3f401d614904725905
ekjamu <ekjamu@gmail.com>
2022-03-11 13:38:50 -0800

    New C Code for Q0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Determine if a random number is positive, negative or zero.
+ * main - Determine if a random number is positive, negative or zerok.
 (*
  * Return: 0 on success
  */
