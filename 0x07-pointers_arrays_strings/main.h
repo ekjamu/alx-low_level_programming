1005724e6a191145e0e552a69981b61a2355086b
ekjamu <ekjamu@gmail.com>
2022-03-28 13:06:10 -0700

    101-C

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,5 +11,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
+abc123
 #endif /* MAIN_H */
