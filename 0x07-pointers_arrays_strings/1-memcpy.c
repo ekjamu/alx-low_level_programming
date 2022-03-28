bde0b9e41321dff63e792c87c107ca6a784b4e94
ekjamu <ekjamu@gmail.com>
2022-03-28 12:39:25 -0700

    Low Level 0x07

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memcpy - Copies memory from one memory location to another memory area.
+ *
+ * @dest: Pointer destination for copied memory area.
+ * @src: Pointer character to be copied.
+ * @n: Number of bytes to be copied.
+ *
+ * Return: Pointer to destination char.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
